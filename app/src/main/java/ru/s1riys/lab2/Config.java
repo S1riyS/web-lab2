a23a2b5bc89c2455f425293a33b95f11909f6aa6
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2024-10-20 17:18:33 +0300

    fix: missing config update

--- app/src/main/java/ru/s1riys/lab2/Config.java
+++ app/src/main/java/ru/s1riys/lab2/Config.java
@@ -3,4 +3,5 @@ package ru.s1riys.lab2;
 public interface Config {
     String RESULT_LIST_BEAN_NAME = "resultListBean";
     String IS_FORWARDED_ATTRIBUTE = "isForwarded";
+    String LAST_RESULTS_UPDATE_TIME = "LAST_RESULTS_UPDATE_TIME";
 }
