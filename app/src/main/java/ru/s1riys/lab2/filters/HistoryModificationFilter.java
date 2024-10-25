7abaf3a90d942d28d825280d2d9daa326a3dd3c7
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2024-10-25 16:16:31 +0300

    chore: clean up

--- app/src/main/java/ru/s1riys/lab2/filters/HistoryModificationFilter.java
+++ app/src/main/java/ru/s1riys/lab2/filters/HistoryModificationFilter.java
@@ -22,15 +22,10 @@ public class HistoryModificationFilter extends HttpFilter {
 
         // Getting If-Modified-Since header
         long ifModifiedSince = request.getDateHeader("If-Modified-Since");
-        System.out.println("If-Modified-Since: ");
-        System.out.println(ifModifiedSince);
 
         // Getting last update time from servlet context
         ServletContext ctx = getServletContext();
         Object lastUpdateTime = ctx.getAttribute(Config.LAST_RESULTS_UPDATE_TIME);
-        System.out.println("Last update time: ");
-        System.out.println(lastUpdateTime);
-        System.out.println("");
 
         if (ifModifiedSince < 0) {
             chain.doFilter(request, response);
@@ -38,7 +33,6 @@ public class HistoryModificationFilter extends HttpFilter {
         }
 
         if (lastUpdateTime == null || (ifModifiedSince > 0 && ifModifiedSince > (long) lastUpdateTime)) {
-            // If results weren't updated at all or were updated after If-Modified-Since
             response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
         } else {
             chain.doFilter(request, response);
